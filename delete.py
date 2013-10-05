d6be9d8a25ab8cb6c6871b99a8bad003bbf0acc2
Thomas Levine <occurrence@thomaslevine.com>
2013-10-05 16:04:34 +0000

    seems to work

--- delete.py
+++ delete.py
@@ -1,11 +1,23 @@
+#!/usr/bin/env python
 import imaplib
 
-def login(mailbox, username, password):
-    mail = imaplib.IMAP4_SSL(mailbox)
-    mail.login(username, password)
-    return mail
+def login(server, username, password):
+    m = imaplib.IMAP4_SSL(server)
+    m.login(username, password)
+    return m
+
+def delete_server_messages(m):
+    typ, data = m.search(None, 'ALL')
+    for num in data[0].split():
+        m.store(num, '+FLAGS', '\\Deleted')
+    m.expunge()
 
 if __name__ == '__main__':
     import sys
-    _, mailbox, username, password = sys.argv
-    login(mailbox, username, password)
+    _, server, username, password = sys.argv
+    folder = sys.argv[4] if len(sys.argv) > 4 else 'INBOX'
+
+    m = login(server, username, password)
+    m.select(folder)
+    delete_server_messages(m)
+    m.close()
